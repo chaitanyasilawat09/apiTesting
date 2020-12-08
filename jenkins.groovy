abb3fa87f095bcef0148cfc0d19bc603bd13c720
chaitanyasilawat09 <chaitanya.liseinfotech@gmail.com>
2020-12-08 19:26:15 +0530

    test

--- jenkins.groovy
+++ jenkins.groovy
@@ -93,7 +93,7 @@ pipeline {
 
 
                       slackSend color: "#FF0000", message: " Build completed and  result:- ${env.JOB_NAME}/${env.BUILD_NUMBER} build started /${env.Build_URL} ......${currentBuild.result}.==============${env.currentResult}"
-                     // notify("${env.JOB_NAME}/${env.BUILD_NUMBER} ...build...  + ${currentBuild.result}")
+                      notify("${env.JOB_NAME}/${env.BUILD_NUMBER} ...build...  + ${currentBuild.result}")
                 }
 
             }
