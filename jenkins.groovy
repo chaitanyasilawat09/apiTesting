a5fdb09a5cb503f7b00ba09c5959647d51d4556d
chaitanyasilawat09 <chaitanya.liseinfotech@gmail.com>
2020-12-11 16:29:43 +0530

    tests

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,7 +1,4 @@
 def branchName = env.BRANCH_NAME
-def Ip4_0Address = "172.18.1.77"
-def branchIpAddress = "172.18.1.153"
-def Ip4_1Address = "172.18.1.65"
 
 def notify(status) {
      slackSend channel: "#jenkinsbuilds",
