162632146c0375f8d820a3654ba085babf632584
chaitanyasilawat09 <chaitanya.liseinfotech@gmail.com>
2020-12-22 22:02:00 +0530

    test

--- src/test/java/AbcTests.java
+++ src/test/java/AbcTests.java
@@ -1,44 +1,41 @@
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.Matchers.hasItems;
-
 public class AbcTests {
 
     @Test
     public void test_1()
     {
-
-                given().
-                get("https://reqres.in/api/users?page=2").
-                then().
-                statusCode(200).
-                body("data.id[1]",equalTo(8)).
-                body ("data.first_name", hasItems("Michael","Lindsay"));
+        System.out.println("hello");
+//                given().
+//                get("https://reqres.in/api/users?page=2").
+//                then().
+//                statusCode(200).
+//                body("data.id[1]",equalTo(8)).
+//                body ("data.first_name", hasItems("Michael","Lindsay"));
 
     }
 
     @Test
     public void test_2()
-    {
-                 given().
-                get("https://reqres.in/api/users?page=2").
-                then().
-                statusCode(200).
-                body("data.id[1]",equalTo(8)).
-                body ("data.first_name", hasItems("Michael","Lindsay"));
+    { System.out.println("hello");
+//                 given().
+//                get("https://reqres.in/api/users?page=2").
+//                then().
+//                statusCode(200).
+//                body("data.id[1]",equalTo(8)).
+//                body ("data.first_name", hasItems("Michael","Lindsay"));
 
     }
     @Test
     public void test_3()
     {
-                given().
-                get("https://reqres.in/api/users?page=2").
-                then().
-                statusCode(200).
-                body("data.id[1]",equalTo(8)).
-                body ("data.first_name", hasItems("Michael","Lindsay"));
+        System.out.println("hello");
+//                given().
+//                get("https://reqres.in/api/users?page=2").
+//                then().
+//                statusCode(200).
+//                body("data.id[1]",equalTo(8)).
+//                body ("data.first_name", hasItems("Michael","Lindsay"));
 
     }
 
@@ -46,12 +43,13 @@ public class AbcTests {
     @Test
     public void test_4()
     {
-                given().
-                get("https://reqres.in/api/users?page=2").
-                then().
-                statusCode(200).
-                body("data.id[1]",equalTo(8)).
-                body ("data.first_name", hasItems("Michael","Lindsay"));
+        System.out.println("hello");
+//                given().
+//                get("https://reqres.in/api/users?page=2").
+//                then().
+//                statusCode(200).
+//                body("data.id[1]",equalTo(8)).
+//                body ("data.first_name", hasItems("Michael","Lindsay"));
 
     }
 }
