13a7d548406029fe03bfecb8e0ebc66b5d26a2de
chaitanyasilawat09 <chaitanya.liseinfotech@gmail.com>
2020-12-22 22:43:30 +0530

    test

--- jenkins.groovy
+++ jenkins.groovy
@@ -6,8 +6,7 @@ def Ip4_0Address = "172.18.1.77"
 def branchIpAddress = "172.18.1.153"
 def Ip4_1Address = "172.18.1.65"
 
-def summary = junit testResults: '/build/reports/tests/runTests/*-reports/TEST-*.xml'
-
+def summary = testng testResults: '/build/reports/tests/runTests/testng-results.xml'
 
 @NonCPS
 def testStatuses() {
