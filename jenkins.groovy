3555c4d202a833fc38385b98abef9621ea971b4a
chaitanyasilawat09 <chaitanya.liseinfotech@gmail.com>
2020-12-22 21:47:24 +0530

    test

--- jenkins.groovy
+++ jenkins.groovy
@@ -127,6 +127,7 @@ pipeline {
 //                        echo "Tests1234: ${testResult1.failCount} / ${testResult1.failureDiffString} failures of ${testResult1.totalCount}.\n\n"
 //                    }
 
+                    slackSend color: "#FF0000", message: " AbstractTestResultAction result is 12343empty "+ testStatuses()"......."
 
                       slackSend color: "#FF0000", message: " Build completed and  result:- ${env.JOB_NAME}/${env.BUILD_NUMBER} build started /${env.Build_URL} ......${currentBuild.result}.==============${env.currentResult}"
                       notify("${env.JOB_NAME}/${env.BUILD_NUMBER} ...build...  + ${currentBuild.result}")
