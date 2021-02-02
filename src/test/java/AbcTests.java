73c187456ab88bab81330792d7ccca64cf670959
chaitanya_silawat <chetanya1992@gmail.com>
2021-02-02 18:14:56 +0530

    ädd test12 brabch

--- src/test/java/AbcTests.java
+++ src/test/java/AbcTests.java
@@ -86,4 +86,15 @@ public class AbcTests {
                 body ("data.first_name", hasItems("Michael","Lindsay"));
 
     }
+    @Test(priority = -899)
+    public void test_6()
+    {
+        given().
+                get("https://reqres.in/api/users?page=2").
+                then().
+                statusCode(200).
+                body("data.id[1]",equalTo(8)).
+                body ("data.first_name", hasItems("Michael","Lindsay"));
+
+    }
 }
